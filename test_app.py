586fd1cd0dcbf0dae5112b59b227e14dfd5cf71d
jcompton24 <comptonjustin18@gmail.com>
2024-09-13 11:21:44 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,8 +2,8 @@ import pytest
 from app import app
 
 @pytest.fixture
-def client():
-    with app.test_client() as client:
+def client()
+    with app.test_client() as client
         yield client
 
 # Home route test
