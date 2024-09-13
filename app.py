578963361daf7de9aa3b54d334f35b0886aef300
jcompton24 <comptonjustin18@gmail.com>
2024-09-13 11:21:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Home(Resource):
-    def get(self):
-        return {'version': '1.0'}
+class Home(Resource)
+    def get(self)
+        return {'version' '1.0'}
 
 class ConvertTemp(Resource):
     def get(self):
